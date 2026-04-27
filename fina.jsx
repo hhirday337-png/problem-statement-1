62f5572a8ffc0de48491b2ebfbdefaff222fc8c2
hhirday337-png <hhirday337@gmail.com>
2026-04-27 01:28:16 -0700

    Add files via upload

---
+++ fina.jsx
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+
+function Users() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch("https://jsonplaceholder.typicode.com/users");
+      const data = await res.json();
+      setUsers(data);
+    }
+
+    fetchData();
+  }, []); 
+
+  return (
+    <div>
+      <h2>User List</h2>
+      {users.map(user => (
+        <p key={user.id}>{user.name}</p>
+      ))}
+    </div>
+  );
+}
+
+export default Users;
